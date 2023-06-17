bac32f6c3f48edc4c17e4a61efc27b8a4cc02153
Sota Matsuo <smatsuo@c1r26s6.42tokyo.jp>
2023-06-17 17:03:29 +0900

    fix

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -4,4 +4,5 @@ ALTER TABLE department_role_member ADD INDEX user_id_mixed_idx(user_id, belong);
 ALTER TABLE department_role_member ADD INDEX role_id_mixed_idx(role_id, belong);
 ALTER TABLE user ADD INDEX office_file_idx(office_id, user_icon_id);
 ALTER TABLE user ADD INDEX mail_pass_idx(mail, password);
-ALTER TABLE user ADD INDEX mail_pass_idx(mail, password);
+-- ALTER TABLE session ADD INDEX linked_user_id_idx(linked_user_id);
+-- ALTER TABLE session ADD INDEX session_id_idx(session_id);
