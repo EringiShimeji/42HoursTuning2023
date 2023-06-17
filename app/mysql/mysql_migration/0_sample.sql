c362c665b6670df2f643cb0b12c76522f4287b70
Sota Matsuo <smatsuo@c1r26s6.42tokyo.jp>
2023-06-17 17:20:46 +0900

    ifx

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -4,5 +4,5 @@ ALTER TABLE department_role_member ADD INDEX user_id_mixed_idx(user_id, belong);
 ALTER TABLE department_role_member ADD INDEX role_id_mixed_idx(role_id, belong);
 ALTER TABLE user ADD INDEX date_kana_idx(entry_date, kana);
 ALTER TABLE user ADD INDEX mail_pass_idx(mail, password);
--- ALTER TABLE session ADD INDEX linked_user_id_idx(linked_user_id);
--- ALTER TABLE session ADD INDEX session_id_idx(session_id);
+ALTER TABLE session ADD INDEX linked_user_id_idx(linked_user_id);
+ALTER TABLE session ADD INDEX session_id_idx(session_id);
