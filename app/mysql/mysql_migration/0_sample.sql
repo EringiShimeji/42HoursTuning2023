187150745decaef7cd3795343d80ab17701c666c
Sota Matsuo <smatsuo@c1r26s6.42tokyo.jp>
2023-06-17 14:40:30 +0900

    fix

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,5 +1,5 @@
 ALTER TABLE match_group_member ADD INDEX user_id_midex_idx(user_id);
-ALTER TABLE department ADD INDEX user_id_midex_idx(user_id);
+ALTER TABLE department ADD INDEX department_id_midex_idx(department_id);
 ALTER TABLE department_role_member ADD INDEX user_id_mixed_idx(user_id, belong);
 ALTER TABLE department_role_member ADD INDEX role_id_mixed_idx(role_id, belong);
 ALTER TABLE user ADD INDEX office_file_idx(office_id, user_icon_id);
